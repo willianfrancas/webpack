92243c381a95904b6f2756be1fd88c7b74050c15
Flávio Almeida <flaviohenriquealmeida@gmail.com>
2021-03-28 14:25:11 +0100

    video2.5

--- webpack.config.js
+++ webpack.config.js
@@ -5,10 +5,12 @@ module.exports = {
     entry: './app/src/js/app.js',
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'app/dist')
+        path: path.resolve(__dirname, 'app/dist'),
+        clean: true
     },
     plugins: [new HtmlWebpackPlugin({
         template: './app/src/app.html',
-        filename: 'app.html'
+        filename: 'app.html',
+        hash: true
     })]
 };
