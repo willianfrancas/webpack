bf4646796efd07b423c6b4150d8b1db91489a3d1
Flávio Almeida <flaviohenriquealmeida@gmail.com>
2021-03-28 14:55:12 +0100

    video3.3

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: './app/src/js/app.js',
@@ -14,11 +13,6 @@ module.exports = {
             template: './app/src/app.html',
             filename: 'app.html',
             hash: true
-        }),
-        new CopyWebpackPlugin({
-            patterns: [
-                { from: './app/src/css', to: 'css'}
-            ]
         })
     ]
 };
