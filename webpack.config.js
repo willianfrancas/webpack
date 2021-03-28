8f609c48a78cc650ddbe81234ae1244422333678
Flávio Almeida <flaviohenriquealmeida@gmail.com>
2021-03-28 15:33:30 +0100

    video3.5

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
     },
     module: {
         rules: [
-            { test: /\.css$/, use: ['css-loader'] }
+            { test: /\.css$/, use: ['style-loader', 'css-loader'] }
         ]
     },
     plugins: [
