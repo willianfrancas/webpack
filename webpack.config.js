57c3b6271eae213069e08f0eba68354df92f803d
Flávio Almeida <flaviohenriquealmeida@gmail.com>
2021-03-28 17:34:38 +0100

    video5.1

--- webpack.config.js
+++ webpack.config.js
@@ -28,13 +28,17 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: './app/src/app.html',
-            filename: 'app.html',
+            template: './app/src/index.html',
+            filename: 'index.html',
             hash: true
         }),
         new MiniCssExtractPlugin({
             filename: 'style.css'
         }),
         new webpack.optimize.ModuleConcatenationPlugin()
-    ]
+    ],
+    devServer: {
+        contentBase: path.resolve(__dirname, 'dist'),
+        port: 3000
+    }
 };
