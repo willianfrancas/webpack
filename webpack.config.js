a6882402652d1f0dbfe7b70d58d34b86aa93a4a0
Flávio Almeida <flaviohenriquealmeida@gmail.com>
2021-03-28 16:23:18 +0100

    video4.5

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,10 @@ module.exports = {
     },
     optimization: {
         minimize: true,
-        minimizer: [new CssMinimizerWebpackPlugin()]
+        minimizer: [
+            new CssMinimizerWebpackPlugin(),
+            '...'
+        ]
     },
     plugins: [
         new HtmlWebpackPlugin({
